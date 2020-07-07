1fbf801f1435d3fc0f0e93c8436cded84500bfd3
Sam Toyer <sam@qxcv.net>
2020-07-06 20:12:27 -0700

    Improve sampler script

--- scripts/sample_trajectories.sh
+++ scripts/sample_trajectories.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env bash
 
 set -e
+
+NTRAJ=50
+DATE="$(date '+%Y-%m-%d')"
 
 get_latest_policy() {
     if [ -z "$1" ]; then
@@ -19,42 +22,44 @@ echo -e "\n\nWorking on cartpole/swingup"
 python collect_demos.py \
     $(cat ./scripts/test_args_ball_reacher_walker_cartpole.sh) \
     "$(get_latest_policy ./rad-out/cartpole-swingup-*-pixel/model/)" \
-    --domain_name cartpole --task_name swingup \
-    --save-path ./rad-out/trajectories/cartpole-swingup.pkl.gz
+    --domain_name cartpole --task_name swingup --ntraj $NTRAJ \
+    --save-path "./rad-out/trajectories/cartpole-swingup-${DATE}.pkl.gz" &
 
 echo -e "\n\nWorking on ball_in_cup/catch"
 python collect_demos.py \
     $(cat ./scripts/test_args_ball_reacher_walker_cartpole.sh) \
     "$(get_latest_policy ./rad-out/ball_in_cup-catch-*-pixel/model/)" \
-    --domain_name ball_in_cup --task_name catch \
-    --save-path ./rad-out/trajectories/ball-in-cup-catch.pkl.gz
+    --domain_name ball_in_cup --task_name catch --ntraj $NTRAJ \
+    --save-path "./rad-out/trajectories/ball-in-cup-catch-${DATE}.pkl.gz" &
 
 echo -e "\n\nWorking on reacher/easy"
 python collect_demos.py \
     $(cat ./scripts/test_args_ball_reacher_walker_cartpole.sh) \
     "$(get_latest_policy ./rad-out/reacher-easy-*-pixel/model/)" \
-    --domain_name reacher --task_name easy \
-    --save-path ./rad-out/trajectories/reacher-easy.pkl.gz
+    --domain_name reacher --task_name easy --ntraj $NTRAJ \
+    --save-path "./rad-out/trajectories/reacher-easy-${DATE}.pkl.gz" &
 
 echo -e "\n\nWorking on walker/walk"
 python collect_demos.py \
     $(cat ./scripts/test_args_ball_reacher_walker_cartpole.sh) \
     "$(get_latest_policy ./rad-out/walker-walk-*-pixel/model/)" \
-    --domain_name walker --task_name walk \
-    --save-path ./rad-out/trajectories/walker-walk.pkl.gz
+    --domain_name walker --task_name walk --ntraj $NTRAJ \
+    --save-path "./rad-out/trajectories/walker-walk-${DATE}.pkl.gz" &
 
 echo -e "\n\nWorking on finger/spin"
 python collect_demos.py \
     $(cat ./scripts/test_args_finger.sh) \
     "$(get_latest_policy ./rad-out/finger-spin-*-pixel/model/)" \
-    --domain_name finger --task_name spin \
-    --save-path ./rad-out/trajectories/finger-spin.pkl.gz
+    --domain_name finger --task_name spin --ntraj $NTRAJ \
+    --save-path "./rad-out/trajectories/finger-spin-${DATE}.pkl.gz" &
 
 echo -e "\n\nWorking on cheetah/run"
 python collect_demos.py \
     $(cat ./scripts/test_args_cheetah.sh) \
     "$(get_latest_policy ./rad-out/cheetah-run-*-pixel/model/)" \
-    --domain_name cheetah --task_name run \
-    --save-path ./rad-out/trajectories/cheetah-run.pkl.gz
+    --domain_name cheetah --task_name run --ntraj $NTRAJ \
+    --save-path "./rad-out/trajectories/cheetah-run-${DATE}.pkl.gz" &
+
+wait
 
 echo "Done, check rad-out/trajectories/"
